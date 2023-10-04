b4bfbd2dcd4d0e850408c3310d0794d70b7721d4
MateoGuerreroE <mateof1223@gmail.com>
2023-10-04 12:32:43 -0500

    Completed up to Relations DB

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,6 +1,8 @@
 const server = require("./app");
 const PORT = 3001;
+const { sequelize } = require("./DB_connection");
 
-server.listen(PORT, () => {
+server.listen(PORT, async () => {
+  await sequelize.sync({ force: true });
   console.log("Server raised in port: " + PORT);
 });
